5a8a590ecb2e05802fe1f1f428e495aa55387b1f
文腾飞 <tengfeiwen@xiaohongshu.com>
2020-05-28 16:37:38 +0800

    feat: 创建XYDemo工程

---
+++ XYDemo/XYDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XYDemo
+//
+//  Created by Coder_WTF on 2020/5/28.
+//  Copyright © 2020 Coder_WTF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XYDemo/XYDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYDemo
+//
+//  Created by Coder_WTF on 2020/5/28.
+//  Copyright © 2020 Coder_WTF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
